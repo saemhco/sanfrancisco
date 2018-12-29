0ed08cfc398dd7e6a22f11bd03f3364454ac2455
slayerderk <slayerderk@gmail.com>
2018-12-29 18:55:17 -0500

    cambios del activo y capa

--- app/Activo.php
+++ app/Activo.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Activo extends Model
 {
-    //
+    protected $table = 'activos';
+    protected $fillable = [
+        'n', 'codigo', 'capa_id', 'nombre', 'unidad', 'costo_unitario', 'dimencion_D', 'dimencion_I', 'dimencion_C', 'dimencion_A', 'dimencion_NR'
+    ];
+    public function capas(){
+        return $this->belongsTo('App\Capa');
+    }   //un Activo, puede pertecer a una CAPA
 }
--- app/Capa.php
+++ app/Capa.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Capa extends Model
 {
-    //
+    protected $table = 'capas';
+    protected $fillable = [
+        'capa', 'descripcion', 'descripcion'
+    ];
+    public function activos()
+    {
+        return $this->hasMany('App\Activos');
+    }   //todo esto indica que una CAPA, pude tener varios Activos
 }
