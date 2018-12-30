7e9cb7f49663115338bfb1ee20302db8d17cddfe
slayerderk <slayerderk@gmail.com>
2018-12-29 21:01:12 -0500

    carazterizacion

--- app/Caracterizacion.php
+++ app/Caracterizacion.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Caracterizacion extends Model
 {
-    //
+    protected $table = 'caracterizacions';
+    protected $fillable = [
+        'activo_id', 'amenaza_id', 'probabilidad', 'dimension_D', 'dimension_I', 'dimension_C', 'dimension_A', 'dimension_NR'
+    ];
+    public function activos(){
+        return $this->hasMany('App\Activo');
+    }   //pude tener varios Activos
+
+    public function amenazas(){
+        return $this->hasMany('App\Amenaza');
+    } 
 }
